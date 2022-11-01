d465c176516de9c4aa9d4666e14103a7f9174c5b
timmarkhuff@gmail.com <timmarkhuff@gmail.com>
2022-10-31 17:21:30 -0700

    Check out the animation!

--- flood_fill.py
+++ flood_fill.py
@@ -1,14 +1,23 @@
 from typing import List
+import cv2
+import numpy as np
+
+w = 255 # wall
+e = 0 # empty
+f = 100 # filled
 
 board = [
-    
-    "......##########......",
-    "......#........#......",
-    "......#........#......",
-    "......#........#####..",
-    "....###............#..",
-    "....#............###..",
-    "....##############....",
+    [e,e,w,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e],
+    [e,e,w,w,w,w,w,w,w,w,w,w,e,e,e,e,e,e,e,e],
+    [e,e,w,e,e,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e],
+    [e,e,w,e,e,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e],
+    [e,e,e,w,e,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e],
+    [e,e,e,w,e,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e],
+    [e,e,e,w,e,e,e,e,e,e,e,w,e,e,e,e,e,e,e,e],
+    [e,e,e,w,w,w,w,w,e,e,e,w,e,e,e,e,e,e,e,e],
+    [e,e,e,e,e,e,e,e,w,w,w,w,e,e,e,e,e,e,e,e],
+    [e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e],
+    [e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e,e],
 ]
 
 positions_to_check = [
@@ -16,68 +25,39 @@ positions_to_check = [
     [0,1], # top
     [1,0], # right
     [0,-1], # bottom
-]
-
-# for viewing printed iterations if needed,
-
-def represent_board(board):
-    for row in board:
-        print(row)      
-    print('------------------------')
+]   
             
-            
-def flood_fill(input_board: List[str], old: str, new: str, x: int, y: int) -> List[str]:
-    """Returns board with old values replaced with new values
-    through flood filling starting from the coordinates x, y
-    Args:
-        input_board (List[str])
-        old (str): Value to be replaced
-        new (str): Value that replaces the old
-        x (int): X-coordinate of the flood start point
-        y (int): Y-coordinate of the flood start point
-    Returns:
-        List[str]: Modified board
-    """
-    #checking if index should be filled
-    if input_board[x][y] == old:
-        input_board[x] = input_board[x][:y] + new + input_board[x][y+1:]
-        represent_board(input_board)
-            
-        
+def flood_fill(input_board: List[str], x: int, y: int) -> List[str]:
+    
     for position in positions_to_check:
         new_x = x + position[0]
         new_y = y + position[1]
+
         # check if out of bounds
-        if new_x >= len(input_board) or new_x < 0:
+        if new_x >= len(input_board[0]) or new_x < 0:
+            continue
+        if new_y >= len(input_board) or new_y < 0:
             continue
             
-        if new_y >= len(input_board[new_x]) or new_y < 0:
-            continue
-            
-        # continue with the fill...
-        
-        if input_board[new_x][new_y] == old:
-             input_board[new_x] = input_board[new_x][:new_y] + new + input_board[new_x][new_y+1:]
-             #recursion case
-             flood_fill(input_board, old, new, new_x, new_y)
-        
-        
+        # checking if pixel is empty and can be filled
+        if input_board[new_y][new_x] == e:
+            input_board[new_y][new_x] = f 
+            # recursion case
+            flood_fill(input_board, new_x, new_y)
+
+        # show the result
+        input_board = np.array(input_board, dtype='uint8')
+        board_to_show = cv2.resize(input_board, (500,500))
+        cv2.imshow('Board', board_to_show)
+        cv2.waitKey(50)
         
     return input_board
-    
-        
+       
+modified_board = flood_fill(input_board=board, x=10, y=10)
 
-modified_board = flood_fill(input_board=board, old=".", new="~", x=5, y=12)
+# cleanup
+cv2.destroyAllWindows()
+print('Done!')
 
-for a in modified_board:
-    print(a)
 
-# Expected output:
-# ......................
-# ......##########......
-# ......#~~~~~~~~#......
-# ......#~~~~~~~~#......
-# ......#~~~~~~~~#####..
-# ....###~~~~~~~~~~~~#..
-# ....#~~~~~~~~~~~~###..
-# ....##############....
+
